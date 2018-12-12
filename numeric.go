af861a8ef750e699bdaa35cb13986bdd18b5c884
Nick Revin <n@nrvn.cc>
2018-12-12 23:49:23 +0300

    replaced numeric cast functions with the spf13/cast

--- numeric.go
+++ numeric.go
@@ -2,78 +2,22 @@ package sprig
 
 import (
 	"math"
-	"reflect"
-	"strconv"
+
+	"github.com/spf13/cast"
 )
 
 // toFloat64 converts 64-bit floats
 func toFloat64(v interface{}) float64 {
-	if str, ok := v.(string); ok {
-		iv, err := strconv.ParseFloat(str, 64)
-		if err != nil {
-			return 0
-		}
-		return iv
-	}
-
-	val := reflect.Indirect(reflect.ValueOf(v))
-	switch val.Kind() {
-	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
-		return float64(val.Int())
-	case reflect.Uint8, reflect.Uint16, reflect.Uint32:
-		return float64(val.Uint())
-	case reflect.Uint, reflect.Uint64:
-		return float64(val.Uint())
-	case reflect.Float32, reflect.Float64:
-		return val.Float()
-	case reflect.Bool:
-		if val.Bool() {
-			return 1
-		}
-		return 0
-	default:
-		return 0
-	}
+	return cast.ToFloat64(v)
 }
 
 func toInt(v interface{}) int {
-	//It's not optimal. Bud I don't want duplicate toInt64 code.
-	return int(toInt64(v))
+	return cast.ToInt(v)
 }
 
 // toInt64 converts integer types to 64-bit integers
 func toInt64(v interface{}) int64 {
-	if str, ok := v.(string); ok {
-		iv, err := strconv.ParseInt(str, 10, 64)
-		if err != nil {
-			return 0
-		}
-		return iv
-	}
-
-	val := reflect.Indirect(reflect.ValueOf(v))
-	switch val.Kind() {
-	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
-		return val.Int()
-	case reflect.Uint8, reflect.Uint16, reflect.Uint32:
-		return int64(val.Uint())
-	case reflect.Uint, reflect.Uint64:
-		tv := val.Uint()
-		if tv <= math.MaxInt64 {
-			return int64(tv)
-		}
-		// TODO: What is the sensible thing to do here?
-		return math.MaxInt64
-	case reflect.Float32, reflect.Float64:
-		return int64(val.Float())
-	case reflect.Bool:
-		if val.Bool() {
-			return 1
-		}
-		return 0
-	default:
-		return 0
-	}
+	return cast.ToInt64(v)
 }
 
 func max(a interface{}, i ...interface{}) int64 {
