a60fe27f46c373dfd23fc3d08cc70aa62f481d19
phuslu <phus.lu@gmail.com>
2020-11-29 15:56:59 +0800

    add all/any for mutiple condition

--- functions.go
+++ functions.go
@@ -219,6 +219,8 @@ var genericMap = map[string]interface{}{
 	"default":          dfault,
 	"empty":            empty,
 	"coalesce":         coalesce,
+	"all":              all,
+	"any":              any,
 	"compact":          compact,
 	"mustCompact":      mustCompact,
 	"fromJson":         fromJson,
