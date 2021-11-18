9b387a1294c24070f1a8586ef927cbd7fe6b1a18
Riccardo Dobloni <riccardo-giovanni.dobloni@1und1.de>
2021-11-18 12:34:18 +0100

    Add error handling for getHostByName function

--- network.go
+++ network.go
@@ -3,10 +3,27 @@ package sprig
 import (
 	"math/rand"
 	"net"
+	"fmt"
+	"time"
 )
 
-func getHostByName(name string) string {
-	addrs, _ := net.LookupHost(name)
-	//TODO: add error handing when release v3 comes out
-	return addrs[rand.Intn(len(addrs))]
+// Error check loop runs 3 times with 15 second intervals only if DNS/connection type errors,
+// if host does not exist "" blank gets returned
+func getHostByName(name string) (string, error) {
+	err := error(nil)
+	for tries := 3; tries > 0; tries-- {
+		addrs, err := net.LookupHost(name)
+		if err == nil {
+			return addrs[rand.Intn(len(addrs))], nil
+		}
+		dnsErr, ok := err.(*net.DNSError)
+		if !ok {
+			return "", fmt.Errorf("DNS Error, %v", err)
+		}
+		if dnsErr.IsNotFound {
+			return "", nil
+		}
+		time.Sleep(15 * time.Second)
+	}
+	return "", fmt.Errorf("Failure in looking up %s: ", err)
 }
